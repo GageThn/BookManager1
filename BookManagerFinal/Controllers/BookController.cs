6a8e5f067ae5efe29953e5e581165ff60a02e7ef
Gage Thorn <gagethorn@Gage.local>
2024-11-07 08:40:04 -0600

    Add ConsoleApp sln

---
+++ BookManagerFinal/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BookManagerFinal.Models;
+namespace BookManagerFinal.Controllers
+{
+	public class BookController : Controller
+	{
+		public BookController()
+		{
+
+		}
+        public IActionResult Index(int id, int rating)
+        {
+            ViewBag.id = id;
+            ViewBag.Name = "Gerald";
+            ViewBag.rating = rating;
+            return View("Index");
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
+
+    }
+}
+
---
+++ BookManagerFinal/Models/BookModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BookManagerFinal.Models
+{
+	public class BookModel
+	{
+	
+		public int id { get; set; }
+		public int rating { get; set; }
+		
+	}
+}
+
