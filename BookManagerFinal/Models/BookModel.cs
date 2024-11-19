46d02b132645829e6f12a8603709febb1b068d62
cbair103 <cbair211@gmail.com>
2024-11-19 08:57:24 -0600

    Update BookModel.cs

--- BookManagerFinal/Models/BookModel.cs
+++ BookManagerFinal/Models/BookModel.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
 namespace BookManagerFinal.Models
 {
 	public class BookModel
 	{
-	
-		public int id { get; set; }
-		public int rating { get; set; }
-		
+		[Key]
+		public int BookId { get; set; }
+
+		[Required(ErrorMessage = "Please enter a name.")]
+		public string Name { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Please a description.")]
+		public string Description { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Please enter a Genre.")]
+		public string Genre { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Please enter the reading status (Read or Will Read).")]
+		public string Status { get; set; } = string.Empty;
+
+		[Range(0,5, ErrorMessage = "Please enter a Rating between 0 and 5.")]
+		public int Rating { get; set; }
+		public string? Slug => Name?.Replace(' ', '-').ToLower();
 	}
 }
 
