0e782817bdb6ea96cffddb30ab162855e08907cb
aliyareed <aliyareed06@gmail.com>
2024-11-20 15:26:35 -0600

    Update HomeController.cs

--- BookManagerFinal/Controllers/HomeController.cs
+++ BookManagerFinal/Controllers/HomeController.cs
@@ -5,12 +5,14 @@ namespace BookManagerFinal.Controllers;
 
 public class HomeController : Controller
 {
+    //dependency injection setup
+    private BookContext context { get; set; }
+    public HomeController(BookContext ctx) => context = ctx;
   
     public IActionResult Index()
     {
-        return View();
+        var books = context.Books.OrderBy(m => m.Name).ToList();
+        return View(books);
     }
-
-
 }
 
