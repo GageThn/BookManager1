9617c9646cdb7e6fb1ffaf4966887c1044f7c8e3
cbair103 <cbair211@gmail.com>
2024-11-21 08:37:07 -0600

    Fixed error

--- BookManagerFinal/obj/Debug/net6.0/BookManagerFinal.AssemblyInfo.cs
+++ BookManagerFinal/obj/Debug/net6.0/BookManagerFinal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookManagerFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fd9b322550f89741be8bf1c9e462e60fd97ef1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dbbc1f0d5e3e9b109646e7e44a10c0f84c2116c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookManagerFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookManagerFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
